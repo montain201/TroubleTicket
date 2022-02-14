fc39f7d58a0a6a1fdbc973a39c678ec63f996c5b
farzaneh.gh <farzaneh.gh@tiddev.com>
2022-02-14 18:04:53 +0330

    Commit

--- Controllers/TicketController.cs
+++ Controllers/TicketController.cs
@@ -43,14 +43,17 @@ namespace Core.Controllers
             //برنامه نویس قادر است تیکت های پروژه خود را رویت کند-u:ghadir-role:developer
             //رول ادمین قادر است تمامی تیکت ها ی پروژه های مختلف را رویت کند-u:admin-role:admin
             //رول مدیر قادر است تیکت های  سایت خود را رویت کند-u:mohiti-role:manager
-            string expertList = string.Empty;
-            if (User.IsInRole("expert"))
-                expertList = "AND USERID = '" + userIdx + "'";
+            //string expertList = string.Empty;
+            //if (User.IsInRole("expert"))
+            //    expertList = "AND USERID = '" + userIdx + "'";
             string query = @" WITH LastTicketStatus AS (
-                              SELECT TS.*,T.TicketNo,T.TicketType, ROW_NUMBER() OVER (PARTITION BY TS.ticketid ORDER BY creationdate DESC) AS RowNo
+                              SELECT TS.TicketStatusId,TS.TicketState,
+                                     TS.TicketStatusDescription,FORMAT(TS.CreationDate, 'yyyy/MM/dd-HH:mm:ss', 'fa')  AS CreationDate,
+                                     T.TicketId,T.TicketNo,T.TicketType,U.UserName, ROW_NUMBER() OVER (PARTITION BY TS.ticketid ORDER BY creationdate DESC) AS RowNo
                               FROM TicketStatus AS TS INNER JOIN Ticket T ON T.TicketId = TS.TicketId 
+                                INNER JOIN   AspNetUsers U ON TS.UserId = U.Id
                             )
-                            SELECT * FROM LastTicketStatus LS WHERE 1>0 "+expertList;
+                            SELECT * FROM LastTicketStatus LS WHERE LS.RowNo = 1 ";
 
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("SQLServerConnection");
@@ -170,8 +173,39 @@ namespace Core.Controllers
         }
         [HttpGet("GetTicketDtl")]
         public JsonResult GetTicketDtl(int tId)
-        {
-            return new JsonResult("Succedd");
+         {        
+            string query = @" WITH LastTicketStatus AS (
+                              SELECT TS.TicketStatusId,TS.TicketState,
+                                     TS.TicketStatusDescription,FORMAT(TS.CreationDate, 'yyyy/MM/dd-HH:mm:ss', 'fa')  AS CreationDate,
+                                     T.TicketId,T.TicketNo,T.TicketType,U.UserName, ROW_NUMBER() OVER (PARTITION BY TS.ticketid ORDER BY creationdate DESC) AS RowNo
+                              FROM TicketStatus AS TS INNER JOIN Ticket T ON T.TicketId = TS.TicketId 
+                                INNER JOIN   AspNetUsers U ON TS.UserId = U.Id
+                            )
+                            SELECT * FROM LastTicketStatus LS WHERE TicketId =  " + tId;
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("SQLServerConnection");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            //foreach (DataRow row in table.Rows)
+            //{
+            //    int val = (int) row["TicketState"];
+            //    var name = Enum.GetName(typeof(TicketState), row["TicketState"]);
+            //    row["TicketState"] = name;
+            //}
+
+            return new JsonResult(table);
         }
 
     }
